ef912fd39b564f5cd03d52125a44c7b2210cd18d
Robert170387 <robert170387@users.noreply.github.com>
2026-07-07 15:54:28 -0500

    diag: personnel columns

--- diag_trips.php
+++ diag_trips.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 require_once 'includes/db.php';
-echo "<b>PERSONNEL columns:</b><br>";
-foreach($pdo->query("DESCRIBE personnel")->fetchAll() as $r) echo $r['Field']."<br>";
+echo '<b>PERSONNEL:</b><br>';
+foreach($pdo->query('DESCRIBE personnel')->fetchAll() as $r) echo $r['Field'].'<br>';

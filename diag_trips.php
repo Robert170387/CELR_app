a24d1d5c2ef7748324ae765587b16c985e66e006
Robert170387 <robert170387@users.noreply.github.com>
2026-07-07 15:52:58 -0500

    diag: check personnel columns

--- diag_trips.php
+++ diag_trips.php
@@ -1,7 +1,4 @@
 ﻿<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 require_once 'includes/db.php';
-echo "Conexion OK<br>";
-$r = $pdo->query("DESCRIBE trips");
-foreach($r->fetchAll() as $row) echo $row['Field'] . "<br>";
+echo "<b>PERSONNEL columns:</b><br>";
+foreach($pdo->query("DESCRIBE personnel")->fetchAll() as $r) echo $r['Field']."<br>";

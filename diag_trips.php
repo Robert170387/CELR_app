5f2e4f19676d76a7125553b83467a67c048649e3
Robert170387 <robert170387@users.noreply.github.com>
2026-07-07 12:43:21 -0500

    diag: test trips table

---
+++ diag_trips.php
@@ -0,0 +1,7 @@
+﻿<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once 'includes/db.php';
+echo "Conexion OK<br>";
+$r = $pdo->query("DESCRIBE trips");
+foreach($r->fetchAll() as $row) echo $row['Field'] . "<br>";
